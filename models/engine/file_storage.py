210262db2eeeb215836ca3e396634f1aa92b7809
Zeezbaba <Azeez_okeleji>
2024-01-10 13:06:47 -0800

    AirBnB_clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -27,6 +27,25 @@ class FileStorage:
             dic = {key: obj.to_dict() for key, obj in FileStorage.__objects.items()}
             json.dump(dic, files)
 
+    def classes(self):
+        """manage correctly serialization and deserialization of all our new classes"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                "User": User
+                "State": State,
+                "City": City,
+                "Amenity": Amenity,
+                "Place": Place,
+                "Review": Review}
+        return classes
+
     def reload(self):
         """deserializes the JSON file to __objects"""
         if not os.path.isfile(FileStorage.__file_path):
@@ -37,3 +56,41 @@ class FileStorage:
                     for key, obj in obj_dict.items()}
             # Initialize FileStorage and calls for reload
             FileStorage.__objects = obj_dict
+
+    def attributes(self):
+        """returns attributes and types"""
+        attributes = {
+                "BaseModel":
+                    {"id": str,
+                    "created_at": datetime.datetime,
+                    "updated_at": datetime.datetime},
+                "User":
+                    {"email": str,
+                        "password": str,
+                        "first_name": str,
+                        "last_name": str},
+                "State":
+                    {"name": str},
+                "City":
+                    {"state_id": str,
+                    "name": str},
+                "Amenity":
+                    {"name": str},
+                "Place":
+                    {"city_id": str,
+                    "user_id": str,
+                    "name": str,
+                    "description": str,
+                    "number_rooms": int,
+                    "number_bathrooms": int,
+                    "max_guest": int,
+                    "price_by_night": int,
+                    "latitude": float,
+                    "longitude": float,
+                    "amenity_ids": list},
+                "Review":
+                    {"place_id": str,
+                        "user_id": str,
+                        "text": str}
+                    }
+        return attributes
