ea56619854ba9db9895fc8c0d64be6d44e81a5ec
Zeezbaba <Azeez_okeleji>
2024-01-10 12:04:40 -0800

    AirBnB_clone

---
+++ models/user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""The user class"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Manages user objects"""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
